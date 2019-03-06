013a3a410ab9cb8ccc76a809d3a560df21d91730
DirkVran <48246976+DirkVran@users.noreply.github.com>
2019-03-06 13:25:07 +0100

    Update home.blade.php
    Nederlandse vertaling toegevoegd

--- backend/resources/views/guests/home.blade.php
+++ backend/resources/views/guests/home.blade.php
@@ -28,12 +28,7 @@
                 <p>
                     Op 2 mei 2019 is 1/3de van mijn leven voorbij:-) Dat moet gevierd worden. We gaan er een leuk feestje
                     van maken. Om het voor iedereen even gezellig te maken, nodig ik jullie allemaal uit om jullie Top 3 van 
-                    meezing- en/of dansliedje door te sturen. 
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard dummy text ever since the Lorem Ipsum has been the industry's standard dummy
-                    text ever since the Lorem Ipsum has been the industry's
-                    standard dummy text ever since the Lorem Ipsum has been the industry's standard dummy text ever
-                    since the
+                    meezing- en/of dansliedje door te sturen.
                 </p>
             </div>
             <div class="side-image-main"><img src="./images/dirk-image2.jpg"></div>
@@ -44,17 +39,17 @@
              -->
         </div>
         <span class="blast-bd">
-            <h2 class="big-italic text-white line-he">My Birthday with a blast!</h2>
+            <h2 class="big-italic text-white line-he">Zing en swing met mij mee!</h2>
         </span>
         <div class="playlist-box">
             <div class="playlist-text">
-                <span>Have the perfect song for the occasion ?</span>
+                <span>Heb je al het perfecte liedje voor mijn verjaardagsfeest?</span>
                 <br>
-                <span>Don't forget to create a playlist for the party</span>
+                <span>Vergeet dan niet om uw playlist aan te maken</span>
             </div>
             <div class="fakeimg">
                 <span>
-                    <a href="{{ route('guest.login') }}">Create a playlist of your favorite songs</a>
+                    <a href="{{ route('guest.login') }}">Maak hier uw playlist aan</a>
                     <img src="./images/Path 31.svg" width="20" height="20"> </a>
                 </span>
             </div>
@@ -65,18 +60,16 @@
 
         <div class="bd-location-box">
             <span class="just-content">
-                <h2 class="big-italic text-white line-he lg-text">Drop your anchors!</h2>
-                <h2 class="big-italic text-white line-he lg-text">And follow the map</h2>
+                <h2 class="big-italic text-white line-he lg-text">Ankers los!</h2>
+                <h2 class="big-italic text-white line-he lg-text">En gebruik deze kaart!</h2>
             </span>
 
             <div class="location-content">
                 <div class="location-tent">
-                    <span class="f-box f-box-label">Location</span>
-                    <span class="f-box f-box-text">Corbie Hotel - Banquet Hall<br> Markt 14, Tessenderlo,</span><br>
-                    <span class="f-box f-box-label">Time</span>
-                    <span class="f-box f-box-text">From 18:00 - 22:00</span><br>
-                    <span class="f-box f-box-label">Attire    </span>
-                    <span class="f-box f-box-text">Semi-Casual</span>
+                    <span class="f-box f-box-label">Locatie</span>
+                    <span class="f-box f-box-text">VVW Zutendaal<br> Sellerveldstraat 10, Zutendaal</span><br>Gelieve de ingang via de Genkerweg te nemen<br>
+                    <span class="f-box f-box-label">Tijdstip</span>
+                    <span class="f-box f-box-text">Vanaf 20:00 - ???</span><br>
                 </div>
                 <div class="g-maps">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10086.871026408058!2d4.992665815508444!3d50.799339073187795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c16cc6612a0117%3A0x2384fb18bd3fba74!2sTotal!5e0!3m2!1sen!2sbe!4v1551104146248"
