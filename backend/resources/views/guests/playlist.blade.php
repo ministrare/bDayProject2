ca09f1a843c5953a88dec149b3543548553ff5c4
DirkVran <48246976+DirkVran@users.noreply.github.com>
2019-03-06 13:36:27 +0100

    Update playlist.blade.php
    Nederlandse vertaling toegevoegd

--- backend/resources/views/guests/playlist.blade.php
+++ backend/resources/views/guests/playlist.blade.php
@@ -8,7 +8,7 @@
                 </span>
             <span class="text-wishes">
                    <h2 class="light-big text-white line-he">50 jaar, dat moet gevierd worden!</h2>
-                <h2 class="big-italic text-white line-he">My Birthday with a blast!</h2>
+                <h2 class="big-italic text-white line-he">Doe mee met meezing- en dansfeest!</h2>
                </span>
             <span class="anchor-box">
                         <img class="r-right" src="/images/Path 34.svg">
@@ -22,7 +22,7 @@
         </div>
 
         <div class="announcement-box big-italic text-white">
-            Add your favorite 3 Songs to the playlist below
+            Voeg uw favoriete Top 3 meezing- en/of dansliedje toe aan de playlist hieronder
         </div>
         <div class="dash">
             <!-- <img src="./images/Line 4.svg">
@@ -33,15 +33,15 @@
             {{ Request::is('playlist')? '' : @method_field('PUT') }}
             <div class="inputBox-mainContent">
                 <div class="field-content">
-                    <span class="small-heading ">Arrgh! Song!</span>
-                    <label for="songName">Artist</label>
+                    <span class="small-heading ">Kom op de party-boot!</span>
+                    <label for="songName">Artiest</label>
                     <input type="text" name="artist" id="artist" class="inp-name" value="{{isset($song->artist) ? $song->artist : old('artist')}}"  placeholder="Just Beiber">
-                    <label for="songName">Song title</label>
+                    <label for="songName">Titel</label>
                     <input type="text" name="songTitle" id="songTitle" class="inp-name" placeholder="Baby" value="{{isset($song->song_title) ? $song->song_title : old('songTitle')}}">
                     <input type="text" name="url" id="songURL" class="inp-name" placeholder="https://youtube.com" hidden value="{{isset($song->url) ? $song->url : old('songURL')}}">
                     <div class="btn-div text-align-right">
                         {{--<button class="button dark">Remove</button>--}}
-                        <button class="button add dark" type="submit">{{Request::is('playlist')? "Add" : "Edit" }}</button>
+                        <button class="button add dark" type="submit">{{Request::is('playlist')? "Toevoegen" : "Wijzigen" }}</button>
                     </div>
                 </div>
                 <div class="preview-content"></div>
@@ -57,8 +57,8 @@
                     <table>
                         <thead>
                             <th>id</th>
-                            <th>artist</th>
-                            <th>song</th>
+                            <th>artiest</th>
+                            <th>titel</th>
                             <th></th>
                         </thead>
                         <tbody>
@@ -68,11 +68,11 @@
                                 <td>{{ $song['artist'] }}</td>
                                 <td>{{ $song['song_title'] }}</td>
                                 <td>
-                                    <a href="{{ route('guest.song.edit', $song) }}" class="button done dark small" type="button">Edit</a>
+                                    <a href="{{ route('guest.song.edit', $song) }}" class="button done dark small" type="button">Wijzig</a>
                                     <form action="{{route('guest.song.destroy', $song['id'])}}" method="POST" style="display: inline-block;">
                                         {{ csrf_field() }}
                                         {{ method_field('DELETE') }}
-                                        <button class="button done dark small">Delete</button>
+                                        <button class="button done dark small">Verwijder</button>
                                     </form>
                                 </td>
                             </tr>
@@ -90,9 +90,9 @@
             @csrf
             <div class="inputBox-mainContent">
                 <div class="field-content">
-                    <span class="small-heading ">Leave a message, Arrgh!</span>
+                    <span class="small-heading ">Ahoy, schrijf hier uw verjaardagswens</span>
                     <textarea name="message" id="" cols="54" rows="10">{{ isset($message) ? $message : old('message') }}</textarea>
-                    <button class="button add dark" type="submit">Submit message</button>
+                    <button class="button add dark" type="submit">Verstuur</button>
                 </div>
             </div>
         </form>
@@ -102,7 +102,7 @@
         </div>
 
         <div class="btn-div bottom text-align-right">
-            <a href="{{ route('guest.done') }}" class="button done dark" type="button">Done</a>
+            <a href="{{ route('guest.done') }}" class="button done dark" type="button">Ik ben klaar</a>
         </div>
     </div>
 @endsection
